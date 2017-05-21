002fd04d4e5914fd5f95d399d3eeeb41a6929b06
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-05-21 12:00:15 +0700

    commit

--- modules/cv/CvModule.php
+++ modules/cv/CvModule.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * CvModule
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra@sudaryanto.id>
+ * @copyright Copyright (c) 2017 Ommu Platform (opensource.ommu.co)
+ * @link https://github.com/ommu/mod-curriculum-vitae
+ * @contact (+62)856-299-4114
+ *
+ *----------------------------------------------------------------------------------------------------------
+ */
 
 class CvModule extends CWebModule
 {
