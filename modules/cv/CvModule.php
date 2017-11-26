11779261da58174eb25385a86dad2fb30c06a666
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-11-26 08:23:04 +0700

    change link repository

--- modules/cv/CvModule.php
+++ modules/cv/CvModule.php
@@ -5,7 +5,7 @@
  *
  * @author Putra Sudaryanto <putra@sudaryanto.id>
  * @copyright Copyright (c) 2017 Ommu Platform (opensource.ommu.co)
- * @link https://github.com/ommu/mod-curriculum-vitae
+ * @link https://github.com/ommu/ommu-cv
  * @contact (+62)856-299-4114
  *
  *----------------------------------------------------------------------------------------------------------
