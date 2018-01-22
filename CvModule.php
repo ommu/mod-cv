2a960e0b83942ab2f67e0e574d94083af57ea066
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-01-22 18:25:47 +0700

    modify module config

--- CvModule.php
+++ CvModule.php
@@ -12,6 +12,9 @@
 
 class CvModule extends CWebModule
 {
+	public $publicControllers = array();
+	private $_module = 'cv';
+	
 	public $defaultController = 'site'; 
 	
 	// getAssetsUrl()
@@ -26,30 +29,40 @@ class CvModule extends CWebModule
 		// import the module-level models and components
 		$this->setImport(array(
 			'cv.models.*',
+			'cv.models.view.*',
 			'cv.components.*',
 		));
-	}
- 
-	public function getAssetsUrl()
-	{
-		if ($this->_assetsUrl === null)
-			$this->_assetsUrl = Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('cv.assets'));
-		
-		return $this->_assetsUrl;
+
+		// this method is called before any module controller action is performed
+		// you may place customized code here
+		// list public controller in this module
+		$publicControllers = array();
+		$controllerMap = array();
+
+		$controllerPath = Yii::getPathOfAlias('application.modules.'.$this->_module.'.controllers');
+		foreach (new DirectoryIterator($controllerPath) as $fileInfo) {
+			if($fileInfo->isDot())
+				continue;
+			
+			if($fileInfo->isFile() && !in_array($fileInfo->getFilename(), array('index.php'))) {
+				$getFilename = $fileInfo->getFilename();
+				$publicControllers[] = $controller = strtolower(preg_replace('(Controller.php)', '', $getFilename));
+				$controllerMap[$controller] = array(
+					'class'=>'application.modules.'.$this->_module.'.controllers.'.preg_replace('(.php)', '', $getFilename),
+				);
+			}
+		}
+		$this->controllerMap = $controllerMap;
+		$this->publicControllers = $publicControllers;
 	}
 
-	public function beforeControllerAction($controller, $action) {
-		if(parent::beforeControllerAction($controller, $action)) {
-			// this method is called before any module controller action is performed
-			// you may place customized code here
-			//list public controller in this module
-			$publicControllers = array(
-				'site',
-			);
-			
+	public function beforeControllerAction($controller, $action) 
+	{
+		if(parent::beforeControllerAction($controller, $action)) 
+		{
 			// pake ini untuk set theme per action di controller..
 			// $currentAction = Yii::app()->controller->id.'/'.$action->id;
-			if(!in_array(strtolower(Yii::app()->controller->id), $publicControllers) && !Yii::app()->user->isGuest) {
+			if(!in_array(strtolower(Yii::app()->controller->id), $this->publicControllers) && !Yii::app()->user->isGuest) {
 				$arrThemes = Utility::getCurrentTemplate('admin');
 				Yii::app()->theme = $arrThemes['folder'];
 				$this->layout = $arrThemes['layout'];
@@ -60,5 +73,13 @@ class CvModule extends CWebModule
 		}
 		else
 			return false;
+	}
+ 
+	public function getAssetsUrl()
+	{
+		if ($this->_assetsUrl === null)
+			$this->_assetsUrl = Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('cv.assets'));
+		
+		return $this->_assetsUrl;
 	}
 }
